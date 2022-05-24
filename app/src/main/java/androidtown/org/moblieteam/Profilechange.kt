561a8f353f74133913fecf86b9895f62dbe7d4b8
Aim-bro <wnsgud970323@gmail.com>
2022-05-24 20:08:02 +0900

    yeeeaaahh

---
+++ app/src/main/java/androidtown/org/moblieteam/Profilechange.kt
@@ -0,0 +1,11 @@
+package androidtown.org.moblieteam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Profilechange : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profilechange)
+    }
+}
